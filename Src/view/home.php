d1d6de128c3feabc8808922bdba270e2c00db25f
TiagoUniverse <tiagocesar68@gmail.com>
2023-08-07 17:14:44 -0300

    Initial commit

---
+++ Src/view/home.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela inicial</title>
+    <link rel="stylesheet" href="../../Assets/Css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <img src="" alt=" logo">
+        <h1>Projeto L.O.U.C.O</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <a>Item</a>
+            <a>Item</a>
+            <a>Item</a>
+            <a>Item</a>
+            <a>Item</a>
+            <a>Item</a>
+        </ul>
+    </nav>
+
+    <div class="main-container">
+        <div class="horizontal-navbar">1</div>
+        <div class="main-screen">2</div>
+    </div>
+</body>
+
+</html>
---
+++ Src/view/login.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../Assets/Css/styles.css">
+    <link rel="stylesheet" href="../../Assets/Css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="image-div">
+            <img src="../../Assets/Img/computer.jpg" alt="A computer">
+        </div>
+
+        <div class="login-form">
+            <img src="" alt =" Logo">
+            <h1>Projeto L.O.U.C.O</h1>
+
+            <form action="home.php" method="GET">
+                <h3 class="email-title">Entre no sistema com a sua conta:</h3>
+                <input type="email" value="" name="email" placeholder="Insira o seu email">
+                <input class="senha-input" type="password" value="" name="senha" placeholder="Insira a sua senha">
+                <button type="submit">Acessar</button>
+            </form>
+            
+            <form>
+            <h3>Novo usuário? Crie a sua conta!</h3>
+                <button type="submit">Criar nova conta</button>
+            </form>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
